5084e8976fc8f6fbcc132a9c9a326e1ce3d28b30
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-13 16:59:30 +0300

    add vue project test_idaproject

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="app">
+		<p>Hello vue</p>
+		<main-wrapper />
+	</div>
+</template>
+
+<script>
+	import MainWrapper from './components/main-wrapper'
+
+export default {
+	name: 'app',
+	components: {
+		MainWrapper
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/main-wrapper.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="main-wrapper"></div>
+</template>
+
+<script>
+	export default {
+		name: "main-wrapper",
+		components: {},
+		props: {},
+		data() {
+			return {
+				title: "main"
+			}
+		},
+		computed: {},
+		methods: {},
+		watch: {},
+		mounted() {
+			console.log('hello Artem')
+		}
+	}
+</script>
+
+<style>
+	.main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		max-width: 1440px;
+		margin: 0 auto;
+	}
+</style>
